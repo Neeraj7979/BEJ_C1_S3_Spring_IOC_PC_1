95197764af107ed59ce9de595717b8b7f57eadce
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 17:49:10 +0530

    Created getter and setter for the fields for the class Department

--- src/main/java/com/niit/bej/domain/Department.java
+++ src/main/java/com/niit/bej/domain/Department.java
@@ -11,5 +11,20 @@ public class Department {
 
     public Department() {
     }
-    
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
