eaea2c9ffbaa939618ac1259d69b974da804bccf
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 01:12:49 +0530

    Created bean for department class and set value of attributes

--- src/main/java/com/niit/bej/beans/ConfigBean.java
+++ src/main/java/com/niit/bej/beans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.niit.bej.beans;
 
+import com.niit.bej.domain.Department;
 import com.niit.bej.domain.Employee;
 import org.springframework.context.annotation.Bean;
 
@@ -10,5 +11,13 @@ public class ConfigBean {
         employee.setEmployeeId(1);
         employee.setEmployeeName("Neera j kumar");
         return employee;
+    }
+
+    @Bean("Department")
+    public Department getDepartmentDetails() {
+        Department department = new Department();
+        department.setDepartmentId(5);
+        department.setDepartmentName("Science");
+        return department;
     }
 }
