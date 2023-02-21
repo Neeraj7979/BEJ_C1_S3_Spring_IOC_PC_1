55adb401edadcf1d301e51a7e6377cbf312ea4c0
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 17:48:38 +0530

    Created constructor for the fields for the class Department

--- src/main/java/com/niit/bej/domain/Department.java
+++ src/main/java/com/niit/bej/domain/Department.java
@@ -4,5 +4,12 @@ public class Department {
     private int departmentId;
     private String departmentName;
 
+    public Department(int departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
 
+    public Department() {
+    }
+    
 }
