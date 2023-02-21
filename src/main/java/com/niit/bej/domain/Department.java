55b3ceb2670c08c61ee1f37777a9102b403e99fe
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 17:47:10 +0530

    Created fields for the class Department

---
+++ src/main/java/com/niit/bej/domain/Department.java
@@ -0,0 +1,8 @@
+package com.niit.bej.domain;
+
+public class Department {
+    private int departmentId;
+    private String departmentName;
+
+
+}
---
+++ src/main/java/com/niit/bej/domain/Employee.java
@@ -0,0 +1,4 @@
+package com.niit.bej.domain;
+
+public class Employee {
+}
