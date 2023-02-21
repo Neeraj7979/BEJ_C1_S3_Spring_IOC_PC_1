554c0f6761494eb3d0cde3764a14e71c2035b82f
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 01:10:32 +0530

    Created bean for employee class and set value of attributes

--- src/main/java/com/niit/bej/beans/ConfigBean.java
+++ src/main/java/com/niit/bej/beans/ConfigBean.java
@@ -1,4 +1,14 @@
 package com.niit.bej.beans;
 
+import com.niit.bej.domain.Employee;
+import org.springframework.context.annotation.Bean;
+
 public class ConfigBean {
+    @Bean("Employee")
+    public Employee getEmployeeDetails() {
+        Employee employee = new Employee();
+        employee.setEmployeeId(1);
+        employee.setEmployeeName("Neera j kumar");
+        return employee;
+    }
 }
